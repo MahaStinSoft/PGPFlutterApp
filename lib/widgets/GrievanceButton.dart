07fbf4942a34b467e2d24e9f6fda0ee4c7e502d9
MahaStinSoft <Mahalakshmi@stinsoft.com>
2024-08-06 23:45:42 +0530

    updated responive UI for home page

--- lib/widgets/GrievanceButton.dart
+++ lib/widgets/GrievanceButton.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/colors.dart';
+import 'package:flutterapp/styles.dart';
 
 class GrievanceButton extends StatelessWidget {
   final IconData icon;
@@ -20,9 +22,9 @@ class GrievanceButton extends StatelessWidget {
       child: GestureDetector(
         onTap: onPressed, // Use this line to handle tap
         child: Container(
-          padding: EdgeInsets.all(12.0),
+          padding: EdgeInsets.all(10.0),
           decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
+            border: Border.all(color: AppColors.hintTextColor),
             borderRadius: BorderRadius.circular(8.0),
           ),
           child: Row(
@@ -30,14 +32,7 @@ class GrievanceButton extends StatelessWidget {
               Icon(icon, color: color, size: 40),
               SizedBox(width: 16.0),
               Expanded(
-                child: Text(
-                  label,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                  ),
-                ),
+                child: Text(label, style: AppTextStyles.labelStyle),
               ),
             ],
           ),
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -6,7 +6,7 @@ import 'package:flutterapp/widgets/GrievanceButton.dart';
 import 'package:flutterapp/widgets/GrievanceCounter.dart';
 import 'package:flutterapp/generated/l10n.dart';
 import 'package:flutterapp/widgets/grievanceFrom.dart';
-import 'package:flutterapp/widgets/myGrievance.dart'; // Ensure you import the GrievancePage
+import 'package:flutterapp/widgets/myGrievance.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -29,116 +29,132 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            height: MediaQuery.of(context).size.height * 0.45,
-            child: Image.asset(
-              'assets/vector.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Column(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final screenWidth = constraints.maxWidth;
+          final screenHeight = constraints.maxHeight;
+
+          return Stack(
             children: [
-              SizedBox(height: 80.0),
-              Center(
-                child: Text(
-                  S.of(context).appTitle,
-                  style: AppTextStyles.headerStyle,
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                height: screenHeight * 0.45,
+                child: Image.asset(
+                  'assets/vector.png',
+                  fit: BoxFit.fill,
                 ),
               ),
-              SizedBox(height: 40.0),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: GrievanceCounter(
-                        count: '0',
-                        label: S.of(context).submittedGrievance,
-                        color: Colors.blue,
-                        borderWidth: 1.5,
-                        backgroundColor: AppColors.textLightColor,
+                    SizedBox(height: screenHeight * 0.1),
+                    Center(
+                      child: Text(
+                        S.of(context).appTitle,
+                        style: AppTextStyles.headerStyle,
                       ),
                     ),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: GrievanceCounter(
-                        count: '0',
-                        label: S.of(context).resolvedGrievance,
-                        color: Colors.green,
-                        borderWidth: 1.5,
-                        backgroundColor: AppColors.textLightColor,
+                    SizedBox(height: screenHeight * 0.05),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: GrievanceCounter(
+                              count: '0',
+                              label: S.of(context).submittedGrievance,
+                              color: Colors.blue,
+                              borderWidth: 1.5,
+                              backgroundColor: AppColors.textLightColor,
+                            ),
+                          ),
+                          SizedBox(width: screenWidth * 0.05),
+                          Expanded(
+                            child: GrievanceCounter(
+                              count: '0',
+                              label: S.of(context).resolvedGrievance,
+                              color: Colors.green,
+                              borderWidth: 1.5,
+                              backgroundColor: AppColors.textLightColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.028),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
+                      child: Column(
+                        children: [
+                          GrievanceButton(
+                            icon: Icons.location_city,
+                            label: S.of(context).knowYourMunicipality,
+                            color: Colors.orange,
+                            onPressed: () {
+                              // Handle navigation or action here
+                            },
+                          ),
+                          SizedBox(height: screenHeight * 0.0),
+                          GrievanceButton(
+                            icon: Icons.edit,
+                            label: S.of(context).newGrievance,
+                            color: Colors.blue,
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => GrievanceFormScreen(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(height: screenHeight * 0.0),
+                          GrievanceButton(
+                            icon: Icons.storage,
+                            label: S.of(context).registeredGrievance,
+                            color: Colors.green,
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => GrievancePage(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(height: screenHeight * 0.0),
+                          GrievanceButton(
+                            icon: Icons.photo,
+                            label: S.of(context).gallery,
+                            color: Colors.yellow,
+                            onPressed: () {
+                              // Handle navigation or action here
+                            },
+                          ),
+                          SizedBox(height: screenHeight * 0.0),
+                          GrievanceButton(
+                            icon: Icons.video_library,
+                            label: S.of(context).videos,
+                            color: AppColors.primaryColor,
+                            onPressed: () {
+                              // Handle navigation or action here
+                            },
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-              SizedBox(height: 10.0),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30.0),
-                  child: ListView(
-                    children: [
-                      GrievanceButton(
-                        icon: Icons.location_city,
-                        label: S.of(context).knowYourMunicipality,
-                        color: Colors.orange,
-                        onPressed: () {
-                          // Handle navigation or action here
-                        },
-                      ),
-                      GrievanceButton(
-                        icon: Icons.edit,
-                        label: S.of(context).newGrievance,
-                        color: Colors.blue,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => GrievanceFormScreen()),
-                          );
-                        },
-                      ),
-                      GrievanceButton(
-                        icon: Icons.storage,
-                        label: S.of(context).registeredGrievance,
-                        color: Colors.green,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => GrievancePage()),
-                          );
-                        },
-                      ),
-                      GrievanceButton(
-                        icon: Icons.photo,
-                        label: S.of(context).gallery,
-                        color: Colors.yellow,
-                        onPressed: () {
-                          // Handle navigation or action here
-                        },
-                      ),
-                      GrievanceButton(
-                        icon: Icons.video_library,
-                        label: S.of(context).videos,
-                        color: AppColors.primaryColor,
-                        onPressed: () {
-                          // Handle navigation or action here
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
             ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
